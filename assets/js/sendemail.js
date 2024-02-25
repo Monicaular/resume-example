d1923f040df88e3c6277d9aaa154b9dcbd97a34a
Monicaular <moniq.iancu@gmail.com>
2024-02-25 11:02:48 +0000

    aDD EMAILjs CODE

---
+++ assets/js/sendemail.js
@@ -0,0 +1,31 @@
+function sendMail(contactForm) {
+    emailjs.send("service_h0iclqq", "rosie", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+}
+
+// function sendMail(contactForm) {
+//     emailjs.send('service_h0iclqq', 'rosie', {
+//         "from_name": contactForm.name.value,
+//         "from_email": contactForm.emailaddress.value,
+//         "project_request": contactForm.projectsummary.value
+//     }).then(
+//     (response) => {
+//       console.log('SUCCESS!', response.status, response.text);
+//     },
+//     (error) => {
+//       console.log('FAILED...', error);
+//     },
+    
+//   );
+// }
